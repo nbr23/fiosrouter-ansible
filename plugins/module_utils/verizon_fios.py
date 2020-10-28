6ad3eaf21792372cf30b163874e79bdac2f8c5c3
Maxence <max@23.tf>
2020-10-28 01:54:45 +0000

    Add module utils, with logon and basic API calls

---
+++ plugins/module_utils/verizon_fios.py
@@ -0,0 +1,87 @@
+import urllib3
+import requests
+from json import JSONDecodeError
+from hashlib import sha512
+
+urllib3.disable_warnings()
+
+def hash_password(password, salt):
+    return sha512(str(password + salt).encode()).hexdigest()
+
+class RouterSession:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.session = requests.Session()
+
+    def create_url(self, path):
+        return 'https://{ip}:{port}/api/{path}'.format(ip=self.ip, port=self.port, path=path)
+
+    def post(self, path, data):
+        try:
+            return self.session.post(self.create_url(path), verify=False, json=data,
+                headers={'X-XSRF-TOKEN': self.session.cookies.get('XSRF-TOKEN')}).json()
+        except JSONDecodeError:
+            pass
+
+    def put(self, path, data):
+        try:
+            return self.session.put(self.create_url(path), verify=False, json=data,
+                headers={'X-XSRF-TOKEN': self.session.cookies.get('XSRF-TOKEN')}).json()
+        except JSONDecodeError:
+            pass
+
+    def get(self, path):
+        try:
+            return self.session.get(self.create_url(path), verify=False,
+                headers={'X-XSRF-TOKEN': self.session.cookies.get('XSRF-TOKEN')}).json()
+        except JSONDecodeError:
+            pass
+
+    def delete(self, path):
+        try:
+            return self.session.delete(self.create_url(path), verify=False,
+                headers={'X-XSRF-TOKEN': self.session.cookies.get('XSRF-TOKEN')}).json()
+        except JSONDecodeError:
+            pass
+
+    def get_password_salt(self):
+        r = self.post('login', None)
+        return r['passwordSalt']
+
+    def login(self, password):
+        r = self.post('login', {'password': hash_password(password, self.get_password_salt())})
+        self.session.cookies = requests.utils.add_dict_to_cookiejar(self.session.cookies, {'bhr4HasEnteredAdvanced': 'true'})
+        return r
+
+    def logout(self):
+        return self.get('logout')
+
+    def get_settings_dns_entries(self):
+        return self.get('settings/dnsserver')
+
+    def post_settings_dns_entry(self, hostname, ipAddress):
+        return self.post('settings/dnsserver',
+                {
+                    'hostname': hostname,
+                    'ipAddress': ipAddress,
+                    'type': 1,
+                    })
+
+    def del_settings_dns(self, entry_id):
+        return self.delete('settings/dnsserver/{}'.format(entry_id))
+
+    def put_settings_dns(self, entry_id, hostname, ipAddress):
+        return self.put('settings/dnsserver/{}'.format(entry_id),
+                {
+                    'hostname': hostname,
+                    'ipAddress': ipAddress,
+                    'type': 1,
+                    })
+
+    def get_settings_dns_hostname(self, hostname):
+        import q
+        for entry in self.get_settings_dns_entries():
+            q(entry)
+            if entry['hostname'] == hostname:
+                return entry
